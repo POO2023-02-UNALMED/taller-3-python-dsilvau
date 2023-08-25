313407800ac74b6c4903aa76a3f150d6359b7e54
dsilvau <125933418+dsilvau@users.noreply.github.com>
2023-08-25 17:51:58 -0500

    4

--- televisores/tv.py
+++ televisores/tv.py
@@ -24,12 +24,12 @@ class TV():
     def getCanal(self):
         return self._canal
     def setCanal(self, canal):
-        if self._estado == True and self._canal >= 1 and self._canal <= 120:
+        if self._estado == True and canal >= 1 and canal <= 120:
             self._canal = canal
     def getVolumen(self):
         return self._volumen
     def setVolumen(self, volumen):
-        if self._estado == True and self._volumen >= 0 and self._volumen <= 7:
+        if self._estado == True and volumen >= 0 and volumen <= 7:
             self._volumen = volumen
     def turnOn(self):
         self._estado = True
