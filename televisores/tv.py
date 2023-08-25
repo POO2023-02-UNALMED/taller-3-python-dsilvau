ecd01b70e5675cf600fcc8ee6d6693192f42f4a7
dsilvau <125933418+dsilvau@users.noreply.github.com>
2023-08-25 18:09:15 -0500

    7

--- televisores/tv.py
+++ televisores/tv.py
@@ -7,7 +7,7 @@ class TV():
         self._precio = 500
         self._volumen = 1
         self._control = None
-        self.numTV += 1
+        numTV += 1
     
     def getMarca(self):
         return self._marca
